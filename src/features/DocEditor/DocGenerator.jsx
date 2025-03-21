7f991cd6aa24efadddd05ab5097e404dcbd7f8ec
saidbek-ai <saidbekxudayberdiyevs@gmail.com>
2025-03-21 23:39:22 +0500

    Adding template to render html

--- src/features/DocEditor/DocGenerator.jsx
+++ src/features/DocEditor/DocGenerator.jsx
@@ -6,53 +6,23 @@ const DocGenerator = ({ htmlContent, wordCount, writingData }) => {
   // const [pdfUrl, setPdfUrl] = useState("");
   const username = useSelector((state) => state.auth.username);
 
-  const element = `
-      <style>
-        body { font-family: Arial, sans-serif; text-color: #111; }
-        h1 { color: #0f766e; text-align: center; font-size: 28px; font-weight: bold; }
-        h2 { color: #0f766e; text-align: center; font-size: 20px; font-weight: bold; }
-        p { font-size: 16px; line-height: 1.6; }
-        ul, ol { padding-left: 20px; }
-        li { font-size: 16px; margin-bottom: 5px; }
-        b, strong { font-weight: bold;}
-        i, em { font-style: italic; }
-        u { text-decoration: underline; }
-        blockquote { font-style: italic; background: #f9f9f9; padding: 10px; border-left: 5px solid #ccc; }
-      </style>
-      <div style="padding: 46px; margin-bottom: 20px;">
-      <span style="font-size: 10px;">formatted by @saidbekxudayberdiyev</span>
-        <div style=" border-bottom: 1px solid #ddd; padding-bottom: 10px; margin-bottom: 20px; display: flex; justify-content: space-between; align-items: center;">
-        
-        <span style="font-size: 22px;">IELTS Writing (${
-          writingData.typeOfWriting === "part_1" ? "Part 1" : "Essay"
-        })</span>
-        <span style="font-size: 16px;">Word Count: ${wordCount}</span>
-        </div>
-        ${
-          writingData.topic
-            ? `<h2 style="padding: 12px 24px;">${writingData?.topic}</h2>`
-            : ""
-        }
-        ${htmlContent}
-
-      ${
-        username
-          ? `<p style="font-size: 16px; padding: 18px 0; text-align: right; font-style: italic;">${username.toUpperCase()}</p>`
-          : ""
-      }
-      </div>
-    `;
-
   const renderDoc = () => {
+    const sheet = document.getElementById("styledSheet");
+
     html2pdf()
       .set({
         padding: 10,
-        filename: `
-        Essay.pdf`,
+        filename: `${
+          writingData.topic
+            ? writingData.topic.slice(0, 12)
+            : writingData.typeOfWritring === "part_1"
+            ? "Report"
+            : "Essay"
+        }.pdf`,
         html2canvas: { scale: 4 },
         jsPDF: { unit: "mm", format: "a4", orientation: "portrait" },
       })
-      .from(element)
+      .from(sheet)
       .toPdf()
       .outputPdf("bloburl") // Get the PDF as a Blob URL
       // .then((pdfUrl) => {
@@ -69,6 +39,29 @@ const DocGenerator = ({ htmlContent, wordCount, writingData }) => {
 
   return (
     <div className="">
+      <template>
+        <div id="styledSheet" className="p-12 mb-5 flex flex-col">
+          <div className="border-b border-gray-900 pb-3 mb-5 flex justify-between items-center">
+            <span className="text-2xl">
+              {writingData.typeOfWriting === "part_1"
+                ? "Task 1 (Report)"
+                : "Task 2 (Essay)"}
+            </span>
+            <span className="text-lg">{wordCount}</span>
+          </div>
+          <div
+            className="mb-4 text-lg"
+            dangerouslySetInnerHTML={{ __html: htmlContent }}
+          ></div>
+          {username ? (
+            <span className="text-lg py-5 text-right italic">
+              {username.toUpperCase()}
+            </span>
+          ) : (
+            ""
+          )}
+        </div>
+      </template>
       <button
         onClick={renderDoc}
         disabled={wordCount === 0}
